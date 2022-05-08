b263a06ea4864106a5c0e7341a3523815cfe0ccf
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-05-08 08:54:33 -0300

    Criado botão de enviar novo feedback

--- src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,6 +1,12 @@
 import { CloseButton } from "../../CloseButton";
 
-export function FeedbackSuccessStep(){
+interface FeedbackSuccessStepProps {
+  onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackSuccessStep({ 
+  onFeedbackRestartRequested
+ }: FeedbackSuccessStepProps){
   return (
    <>
     <header>
@@ -13,9 +19,11 @@ export function FeedbackSuccessStep(){
       </svg>
       <span className="text-xl mt-2">Agradecemos o feedback!</span>
       <button
+        type="button"
+        onClick={onFeedbackRestartRequested}
         className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 disabled:opacity-50 disabled:hover:bg-brand-500"
       >
-
+        Quero enviar outro
       </button>
     </div>
    </>
